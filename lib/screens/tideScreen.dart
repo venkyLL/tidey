3b8f826dc93f745743a3b7cb0862e76271e17143
Ted Dimbero <ted@amberjacklabs.com>
2021-04-18 07:57:50 -0500

    Tide Table has real data

--- lib/screens/tideScreen.dart
+++ lib/screens/tideScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tidey/components/subTile.dart';
 import 'package:tidey/components/zeClock.dart';
 import 'package:tidey/const.dart';
 import 'package:tidey/screens/moonScreen.dart';
@@ -11,18 +10,22 @@ class TideScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          // AppBar(title: Text(globalLatitude == null ? "bob" : globalLatitude)),
-          AppBar(
-        title: Text(globalLatitude == null ? "Tide" : globalLatitude),
-        centerTitle: true,
-        leading: GestureDetector(
-          onTap: () {/* Write listener code here */},
-          child: Icon(
-            Icons.menu, // add custom icons also
+        extendBodyBehindAppBar: true,
+        appBar:
+            // AppBar(title: Text(globalLatitude == null ? "bob" : globalLatitude)),
+            AppBar(
+          //    title: Text(globalLatitude == null ? "Tide" : globalLatitude),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          //  backgroundColor: Color(0x44000000),
+          elevation: 0,
+          leading: GestureDetector(
+            onTap: () {/* Write listener code here */},
+            child: Icon(
+              Icons.menu, // add custom icons also
+            ),
           ),
-        ),
-        actions: <Widget>[
+          actions: <Widget>[
 //          Padding(
 //              padding: EdgeInsets.only(right: 20.0),
 //              child: GestureDetector(
@@ -32,25 +35,144 @@ class TideScreen extends StatelessWidget {
 //                  size: 26.0,
 //                ),
 //              )),
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, MoonScreen.id);
+                  },
+                  child: Icon(Icons.chevron_right),
+                )),
+          ],
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/blueTexture.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.white.withOpacity(0.8), BlendMode.dstATop),
+            ),
+          ),
+          constraints: BoxConstraints.expand(),
+          child: Column(
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(height: 50),
+                zeClock(),
+                SizedBox(height: 20),
+                buildTideTable(),
+              ]),
+        ));
+  }
+
+  Container buildTideTable() {
+    return Container(
+      child: Column(
+        children: [
+          Text("Tides", style: kTableTitleTextStyle),
+          SizedBox(height: 10),
           Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, MoonScreen.id);
-                },
-                child: Icon(Icons.chevron_right),
-              )),
+            padding: const EdgeInsets.only(left: 65, right: 65),
+            child: Table(
+//            columnWidths: {
+//          0: FractionColumnWidth(.32),
+//          1: FractionColumnWidth(.33),
+//          2: FractionColumnWidth(.30),
+//          //   3: FractionColumnWidth(.05),
+//        } ,
+                children: [
+                  tideTableRow(
+                      day: 'Today',
+                      time: weatherData
+                          .data.weather[0].tides[0].tideData[0].tideTime,
+                      level: (double.parse(weatherData
+                          .data.weather[0].tides[0].tideData[0].tideHeightMt)),
+                      direction: weatherData
+                          .data.weather[0].tides[0].tideData[0].tideType),
+                  tideTableRow(
+                      day: 'Today',
+                      time: weatherData
+                          .data.weather[0].tides[0].tideData[1].tideTime,
+                      level: (double.parse(weatherData
+                          .data.weather[0].tides[0].tideData[1].tideHeightMt)),
+                      direction: weatherData
+                          .data.weather[0].tides[0].tideData[1].tideType),
+                  tideTableRow(
+                      day: 'Tomorrow',
+                      time: weatherData
+                          .data.weather[0].tides[0].tideData[2].tideTime,
+                      level: (double.parse(weatherData
+                          .data.weather[0].tides[0].tideData[2].tideHeightMt)),
+                      direction: weatherData
+                          .data.weather[0].tides[0].tideData[2].tideType),
+                  tideTableRow(
+                      day: 'Tomorrow',
+                      time: weatherData
+                          .data.weather[0].tides[0].tideData[3].tideTime,
+                      level: (double.parse(weatherData
+                          .data.weather[0].tides[0].tideData[3].tideHeightMt)),
+                      direction: weatherData
+                          .data.weather[0].tides[0].tideData[3].tideType),
+                ]),
+            // mySubTile(kMySubTileData),
+          ),
         ],
       ),
-      body: SafeArea(
-        child: Column(
-//            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              zeClock(),
-              mySubTile(kMySubTileData),
-            ]),
-      ),
+    );
+  }
+
+// 3.28084
+  TableRow tideTableRow(
+      {String day, String time, double level, String direction}) {
+    print("Direction is $day,$time, $level,$direction");
+    // static htInFeet = double.parse(level)/3.28084;
+    final String pos = level > 2.0 ? "+" : "-";
+    return TableRow(
+      children: [
+        Text(day, style: kTableTextStyle),
+        Text(
+          time,
+          style: kTableTextStyle,
+          textAlign: TextAlign.right,
+        ),
+        direction == "LOW"
+            ? RichText(
+                textAlign: TextAlign.right,
+                text: TextSpan(
+                  /*defining default style is optional */
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: ((level * 3.28084).toStringAsFixed(2)) + 'ft ',
+                      style: kTableTextStyle,
+                    ),
+                    TextSpan(text: 'L', style: kTableTextStyleRed),
+                  ],
+                ),
+              )
+            : RichText(
+                textAlign: TextAlign.right,
+                text: TextSpan(
+                  /*defining default style is optional */
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: ((level * 3.28084).toStringAsFixed(2)) + 'ft ',
+                      style: kTableTextStyle,
+                    ),
+                    TextSpan(text: 'H', style: kTableTextStyleGreen),
+                  ],
+                ),
+              ),
+
+//        Text(
+//            //  (level > 0.0 ? "+" : "-") +
+//            ((level * 3.28084).toStringAsFixed(2)) + 'ft',
+//            style: kClockTextSmallStyle),
+//        direction == "LOW"
+//            ? Text("L", style: kTableTextStyleRed)
+//            : Text("H", style: kTableTextStyleGreen),
+      ],
     );
   }
 }
